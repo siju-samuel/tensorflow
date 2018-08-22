a3acd5965bd0948ea19f57c473c6d834e7c2d0ea
Siju Samuel <sijusamuel@gmail.com>
2018-08-22 05:05:12 +0000

    Synced for local update from master/origin

--- tensorflow/compiler/tests/reshape_op_test.py
+++ tensorflow/compiler/tests/reshape_op_test.py
@@ -15,6 +15,8 @@
 """Tests for slicing."""
 
 from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
 
 from absl.testing import parameterized
 
