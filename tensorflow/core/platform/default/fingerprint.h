d1a34cf513bc51296da9959ed7e8da2403f5bc2b
Siju Samuel <sijusamuel@gmail.com>
2019-09-08 05:05:15 +0000

    Synced for local update from master/origin

--- tensorflow/core/platform/default/fingerprint.h
+++ tensorflow/core/platform/default/fingerprint.h
@@ -1,4 +1,4 @@
-/* Copyright 2016 The TensorFlow Authors. All Rights Reserved.
+/* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -17,8 +17,6 @@ limitations under the License.
 #define TENSORFLOW_CORE_PLATFORM_DEFAULT_FINGERPRINT_H_
 
 #include <farmhash.h>
-
-#include "tensorflow/core/lib/core/stringpiece.h"
 
 namespace tensorflow {
 
